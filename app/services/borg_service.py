f10323afa7fe3d2b85f587358438375d243aeed5
Jam Risser <jam@jamrizzi.com>
2017-09-09 20:55:09 -0500

    Improved borg service

--- app/services/borg_service.py
+++ app/services/borg_service.py
@@ -10,28 +10,47 @@ class Borg():
     @staticmethod
     def init(repo_path, passphrase=None, verbose=False):
         self = Borg(repo_path, passphrase=passphrase, verbose=verbose)
-        command = None
+        repo_path = path.abspath(path.join(os.getcwd(), repo_path))
+        pipe_list = None
+        pargs = {
+            'verbose': self.verbose
+        }
         if passphrase:
-            command = '(echo ' + passphrase + '; echo ' + passphrase + '; echo) | ';
-        command = 'borg init '
+            pipe_list = [
+                'echo ' + passphrase,
+                'echo ' + passphrase,
+                'echo'
+            ]
         if not passphrase:
-            command += ' --encryption=none'
-        command += (' -v' if self.verbose else '') + ' ' + path.abspath(path.join(os.getcwd(), repo_path))
-        os.system(command)
+            pargs['encryption'] = 'none'
+        self.command(
+            'init',
+            pargs,
+            path=repo_path,
+            pipe_list=pipe_list
+        )
         return self
 
     def create(self, backup_name, backup_location):
-        return self.system(
+        backup_location = path.abspath(path.join(os.getcwd(), backup_location))
+        return self.command(
             'create',
-            '--list ::' + backup_name + ' ' + path.abspath(path.join(os.getcwd(), backup_location))
+            {
+                'verbose': self.verbose,
+                'list': True
+            },
+            backup_name=backup_name,
+            path=backup_location
         )
 
     def extract(self, backup_name, extract_location, extract_from):
-        cwd = os.getcwd()
-        os.chdir(extract_location)
-        response = self.system('extract', '::' + backup_name + ' ' + extract_from)
-        os.chdir(cwd)
-        return response
+        return self.command(
+            'extract',
+            {'verbose': self.verbose},
+            backup_name=backup_name,
+            path=extract_from,
+            working_dir=extract_location
+        )
 
     def check(self):
         pass
@@ -42,9 +61,15 @@ class Borg():
     def list(self, backup_name=None):
         response = None
         if backup_name:
-            response = self.system('list', '--short ::' + backup_name).split('\n')
+            response = self.command('list', {
+                'verbose': self.verbose,
+                'short': True
+            }, backup_name).split('\n')
         else:
-            response = self.system('list', '--short').split('\n')
+            response = self.command('list', {
+                'verbose': self.verbose,
+                'short': True
+            }).split('\n')
         return response[:len(response) - 1]
 
     def delete(self):
@@ -80,10 +105,53 @@ class Borg():
     def breakLock(self):
         pass
 
-    def system(self, command, args=''):
+    def command(self, command, pargs=None, backup_name=None, path=None, working_dir=None, pipe_list=None):
+        if pargs:
+            for parg, value in pargs.iteritems():
+                command += encode_parg_str(parg, value)
+        if backup_name:
+            command += ' ::' + backup_name
+        if path:
+            command += ' ' + path
+        return self.system(command, pipe_list, working_dir)
+
+    def system(self, command, pipe_list=None, working_dir=None):
+        cwd = None
+        if working_dir:
+            cwd = os.getcwd()
+            os.chdir(working_dir)
         os.environ['BORG_REPO'] = path.abspath(path.join(os.getcwd(), self.path))
         if self.passphrase:
             os.environ['BORG_PASSPHRASE'] = self.passphrase
         elif 'BORG_PASSPHRASE' in os.environ:
             del os.environ['BORG_PASSPHRASE']
-        return os.popen('borg ' + command + (' -v' if self.verbose else '') + ' ' + args).read()
+        command = 'borg ' + command
+        if pipe_list:
+            pipe = '('
+            for item in pipe_list:
+                pipe += item + '; '
+            pipe = pipe[:len(pipe) - 2] + ') | '
+            command = pipe + command
+        if 'DEBUG' in os.environ and os.environ['DEBUG'].lower() == 'true':
+            print('$ ' + command + '\n')
+        response = os.popen(command).read()
+        if working_dir:
+            os.chdir(cwd)
+        return response
+
+def encode_parg_str(parg, value):
+    if value == None:
+        return ''
+    parg_str = '-'
+    if len(parg) > 1:
+        parg_str += '-'
+    parg_str += parg
+    if type(value) == bool:
+        if value:
+            return ' ' + parg_str
+        else:
+            return ''
+    elif type(value) == str:
+        return ' ' + parg_str + '=' + value
+    else:
+        return ' ' + parg_str + '=' + str(value)
